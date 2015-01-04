670fbed707436287971c63730ee6a891ba27e871
Maximilian Schremser <maximilian.schremser@at.ibm.com>
2015-01-04 18:02:58 +0100

    add config.properties and morgan logger
    * remove bodyParser
    * remove errorHandling
    * implemented stormpath

--- app.js
+++ app.js
@@ -2,14 +2,10 @@ var express = require('express');
 var path = require('path');
 //var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
 var stormpath = require('express-stormpath');
-//var routes = require('./routes/index');
-//var users = require('./routes/users');
 var profile = require('./routes/profile')();
-// var secretkey = require('secretkey');
-
+var properties = require('properties');
+var application = "", secretKey = "";
 var app = express();
 
 // view engine setup
@@ -19,54 +15,33 @@ app.set('view engine', 'jade');
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+// read api endpoint and secretKey from configFile
+properties.parse('./config.properties', {path: true}, function(err, cfg) {
+  if (err) {
+    console.error('A file named config.properties containing the API endpoint is missing.');
+    console.error('The file must contain the following properties: apikey and apitoken.');
+    throw e;
+  }
+  application = cfg.application;
+  secretKey = cfg.secretKey;
 });
 
 var stormpathMiddleware = stormpath.init(app, {
   apiKeyFile: path.join(__dirname, '.stormpath') + '/apiKey.properties',
-  application: 'https://api.stormpath.com/v1/applications/6MtnbKJ9p5FtISeUK7KItV',
-  secretKey: 'changeit',
+  application: application,
+  secretKey: secretKey,
   expandCustomData: true,
   enableForgotPassword: true
 });
 
 app.use(stormpathMiddleware);
 
-//app.use('/', routes);
-//app.use('/users', users);
 app.use('/profile', profile);
 
+// send the index view on start
+app.get('/', function(req, res) {
+  res.render('index', {title: "SmartGuard"});
+});
 module.exports = app;
