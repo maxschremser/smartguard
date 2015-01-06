9b6017cc9f6e3301dd244b023eae0f66c5dffc54
Maximilian Schremser <maximilian.schremser@at.ibm.com>
2015-01-06 19:27:30 +0100

    additional images and css

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,12 @@ var express = require('express');
 
 /* GET home page. */
 module.exports = function index() {
+  console.log("index route setup");
   var router = express.Router();
 
   router.get('/', function (req, res) {
     res.render('index', {title: 'SmartGuard Web Control'});
   });
 
-  console.log("index route setup");
   return router;
 }
